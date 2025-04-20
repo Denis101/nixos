08bb4535d3b1a9555b9352dd0b978d208fefe8ee
Denis Craig <admin@deniscraig.com>
2025-04-20 22:35:53 +0100

    Updated flake dependencies, replaced lib with nix-utils

--- flake.nix
+++ flake.nix
@@ -1,106 +1,158 @@
 {
-  description = "Global development environment";
+  description = "NixOS configurations, modules, and devShells.";
 
   inputs = {
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
-    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.*";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.*";
+    flake-schemas = {
+      type = "github";
+      owner = "DeterminateSystems";
+      repo = "flake-schemas";
+      ref = "refs/tags/v0.1.5";
+    };
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      ref = "refs/tags/v1.0.0";
+    };
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "24.11";
+    };
+    nix-fmt = {
+      type = "github";
+      owner = "Denis101";
+      repo = "flake-nix-fmt";
+      ref = "0.0.2";
+      inputs.flake-schemas.follows = "flake-schemas";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-utils = {
+      type = "github";
+      owner = "Denis101";
+      repo = "flake-nix-utils";
+      ref = "0.0.1";
+      inputs.flake-schemas.follows = "flake-schemas";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-fmt.follows = "nix-fmt";
+    };
     wsl = {
-      url = "github:nix-community/NixOS-WSL";
+      type = "github";
+      owner = "nix-community";
+      repo = "NixOS-WSL";
+      ref = "2411.6.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
-      url = "github:lnl7/nix-darwin/master";
+      type = "github";
+      owner = "nix-darwin";
+      repo = "nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mac-app-util = {
-      url = "github:hraban/mac-app-util";
+      type = "github";
+      owner = "hraban";
+      repo = "mac-app-util";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
     home-manager = {
       type = "github";
       owner = "nix-community";
       repo = "home-manager";
       ref = "release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, ... }@inputs: rec {
+  outputs = {
+    self,
+    nixpkgs,
+    nix-fmt,
+    nix-utils,
+    ...
+  } @ inputs: rec {
     inherit (self) outputs;
     schemas = inputs.flake-schemas.schemas;
-    lib = import ./lib.nix inputs;
+    checks = nix-fmt.checks;
+    formatter = nix-fmt.formatter;
 
-    nixosConfigurations = lib.flattenAttrset (
+    nixosConfigurations = nix-utils.lib.flattenAttrset (
       builtins.mapAttrs (
         system: platforms:
-        builtins.mapAttrs (
-          name: module:
-          lib.buildNixos {
-            inherit system module;
-            specialArgs = { inherit inputs outputs; };
-          }
-        ) platforms
-      ) lib.linuxPlatforms
+          builtins.mapAttrs (
+            name: module:
+              nix-utils.lib.buildNixos {
+                inherit system module;
+                specialArgs = {inherit inputs outputs;};
+              }
+          )
+          platforms
+      )
+      nix-utils.lib.linuxPlatforms
     );
 
-    darwinConfigurations = lib.flattenAttrset (
+    darwinConfigurations = nix-utils.lib.flattenAttrset (
       builtins.mapAttrs (
         system: platforms:
-        builtins.mapAttrs (
-          name: module:
-          lib.buildDarwin {
-            inherit system module;
-            specialArgs = { inherit inputs outputs; };
-          }
-        ) platforms
-      ) lib.darwinPlatforms
+          builtins.mapAttrs (
+            name: module:
+              nix-utils.lib.buildDarwin {
+                inherit system module;
+                specialArgs = {inherit inputs outputs;};
+              }
+          )
+          platforms
+      )
+      nix-utils.lib.darwinPlatforms
     );
 
-    homeModules = builtins.mapAttrs (
-      system: platforms:
+    homeModules =
       builtins.mapAttrs (
-        name: module: (builtins.head (lib.attrsToList module.home-manager.users)).value
-      ) platforms
-    ) lib.platforms;
+        system: platforms:
+          builtins.mapAttrs (
+            name: module: (builtins.head (nixpkgs.lib.attrsToList module.home-manager.users)).value
+          )
+          platforms
+      )
+      nix-utils.lib.platforms;
 
-    homeConfigurations = builtins.mapAttrs (
-      system: platforms:
+    homeConfigurations =
       builtins.mapAttrs (
-        name: module:
-        lib.buildHome {
-          inherit system module;
-          specialArgs = { inherit inputs outputs; };
-        }
-      ) platforms
-    ) homeModules;
+        system: platforms:
+          builtins.mapAttrs (
+            name: module:
+              nix-utils.lib.buildHome {
+                inherit system module;
+                specialArgs = {inherit inputs outputs;};
+              }
+          )
+          platforms
+      )
+      homeModules;
 
-    packages = lib.forAllSystems (
-      system:
-      {
+    packages = nix-utils.lib.forAllSystems (
+      system: {
         inherit nixosConfigurations darwinConfigurations;
         homeConfigurations = homeConfigurations.${system};
       }
     );
 
-    devShells = lib.forAllSystems (
-      system:
-        let pkgs = import nixpkgs { inherit system; };
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              git
-              nixfmt
-              shfmt
-              shellcheck
-            ];
-          };
-        }
-    );
-
-    formatter = lib.forAllSystems (
-      system:
-      let pkgs = import nixpkgs { inherit system; };
-      in
-      pkgs.nixfmt-rfc-style
+    devShells = nix-utils.lib.forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            nixfmt
+            shfmt
+            shellcheck
+          ];
+        };
+      }
     );
   };
 }
